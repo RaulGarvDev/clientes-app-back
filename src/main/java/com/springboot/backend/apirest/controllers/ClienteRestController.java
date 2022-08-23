a271a8702e7df1f7a904ae7b90d7491a570104d4
RaulGarvDev <raulgarvdev@gmail.com>
2022-08-23 20:31:43 +0200

    Sección 81

--- src/main/java/com/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -4,13 +4,17 @@ package com.springboot.backend.apirest.controllers;
 import com.springboot.backend.apirest.models.entity.Cliente;
 import com.springboot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
+import javax.validation.Valid;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -30,15 +34,54 @@ public class ClienteRestController {
     @GetMapping("/clientes/{id}")
     public ResponseEntity<Cliente> show(@PathVariable Long id) {
 
+        System.out.println(clienteService.findById(id));
         return  new ResponseEntity<Cliente>(clienteService.findById(id), HttpStatus.OK);
     }
 
 
     @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
+    public ResponseEntity<?>create(@Valid @RequestBody Cliente cliente, BindingResult result){
 
-        return  clienteService.save(cliente);
+        Cliente clienteNew = null;
+
+        Map<String, Object> response = new HashMap<>();
+
+        if(result.hasErrors()){
+
+           // List<String> errors1 = new ArrayList<>();
+
+          /*  for (FieldError err : result.getFieldErrors()){
+                errors.add("El campo" + err.getField()+" "+err.getDefaultMessage());
+            }*/
+
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map( err -> "El campo"+ err.getField()+" "+ err.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            response.put("error", errors);
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+
+        try{
+
+            clienteNew = clienteService.save(cliente);
+
+
+        }catch(DataAccessException e){
+
+            response.put("mensaje", "Error al realizar el insert en la bbdd");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "el cliente ha sido creado con exito");
+        response.put("cliente", clienteNew);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/clientes/{id}")
--- src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
@@ -2,8 +2,12 @@ package com.springboot.backend.apirest.models.entity;
 
 import lombok.Data;
 import lombok.Generated;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -17,8 +21,19 @@ public class Cliente implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+
+    @NotEmpty(message = "No puede estar vacio")
+    @Size(min = 4, max = 12, message = "Entre 4 y 12 carácteres")
+    @Column (nullable=false)
     private String nombre;
+
+    @NotEmpty(message = "No puede estar vacio")
     private String apellido;
+
+    @NotEmpty(message = "No puede estar vacio")
+    @Email(message = "No es una dirección de email")
+    @Column (nullable=false)
     private String email;
 
     @Column(name = "create_at")
